41c2a5578e376a7820e647b7bbb5bc46de72ac79
phosphene <ed@cronos.net>
2014-05-21 17:00:33 -0400

    add discussion of Tuple

--- src/cascalog_test_demo/core.clj
+++ src/cascalog_test_demo/core.clj
@@ -8,6 +8,15 @@
             [clojure.string :as s]))
 
 
+;; In Cascalog as in Cascading the basic data structure is the tuple
+;; Tuples are distinguished from sets for example
+;; A tuple may contain multiple instances of the same element, so
+;; tuple (1,2,2,3) \neq (1,2,3); but set \{1,2,2,3\} = \{1,2,3\}.
+;; Tuple elements are ordered: tuple (1,2,3) \neq (3,2,1), but set \{1,2,3\} = \{3,2,1\}.
+;; A tuple has 'n' number of elements
+;; another way of reasoning about tuples is as an abstraction from "multiple"
+;; n-mutiple is n-tuple 
+
 ;;demonstrate basic query syntax
 ;; query creation operator: <- 
 ;; what we want: [?x ?sum]
@@ -21,4 +30,19 @@
       (src ?x ?y)
       (:sort ?x)
       (c/sum ?y :> ?sum)))
+
+
+;;latest cascalog api
+(def/defmapcatfn tokenise [line]
+  "reads in a line of string and splits it by a regular expression"
+  (s/split line #"[\[\]\\\(\),.)\s]+"))
+
+;;demonstrate word count
+
+(defn word-count-tokenise [src]
+  "word count and split each line in tuples"
+  (<- [?word ?count]
+      (src ?line)
+      (tokenise ?line :> ?word)
+      (c/count ?count)))
 
